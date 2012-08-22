626e7dea00c6dad5069f2f5eef34479025235c20
Ben Argo <ben@benargo.com>
2012-08-22 03:22:35 +0100

    Small update to battle.net usage information so it reads better

--- www/legal/battlenet.php
+++ www/legal/battlenet.php
@@ -20,7 +20,7 @@ require(PATH.'framework/head.php');
 
 <p>Some of the services provided by Ashkandari Web Services (hereafter "Ashkandari"), a non-profit individual operation operating within the United Kingdom of Great Britain and Northern Ireland (hereafter the "United Kingdom"), use data obtained from the Blizzard Community Platform API, and is subject to their <a href="http://blizzard.github.com/api-wow-docs/#idp57480" target="_blank">API Policy</a>.</p>
 
-<p>We are obligated to provide the source code that communicates with Battle.net. You can find this code and its development history published online at <a href="https://github.com/benargo/Ashkandari/">https://github.com/benargo/Ashkandari/</a>.</p>
+<p>We are obligated to provide the source code that communicates with Battle.net. You can find this code and its development history <a href="https://github.com/benargo/Ashkandari/">published online</a>.</p>
 
 <p>We are obligated to disclaim any association with, or endorsement by, Blizzard Entertainment SAS. All related data, materials, logos, and images are copyright &copy; Blizzard Entertainment SAS.</p>
 
