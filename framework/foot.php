7249aa6fc2cd22c083370a48314b1bd605e05430
Ben Argo <ben@beanrgo.com>
2012-08-22 21:29:20 +0100

    Introduction of mobile stylesheet

--- framework/foot.php
+++ framework/foot.php
@@ -6,7 +6,13 @@
 		</div>
 		<div id="footer-wrapper">
 			<footer>
-				<p>Copyright &copy; <?php echo date('Y'); ?> Ashkandari // <a href="/rules/" title="Our guild rules governs your behaviour within our guild">Guild Rules</a> // <a href="/legal/terms" title="These terms outline and govern your usage of our websites.">Terms of Service</a> // <a href="/legal/privacy" title="Our policies regarding data protection, privacy and our use of cookies.">Privacy &amp; Cookies</a> // <a href="/legal/battlenet" title="Our policies and source code relating to our usage of the battle.net API">Battle.net Usage Information</a> // <a href="http://www.benargo.co.uk/" target="_blank">Website by Ben Argo</a></p>
+				<ul id="footerlinks">
+					<li>Copyright &copy; <?php echo date('Y'); ?> Ashkandari</li>
+					<li><a href="/rules/" title="Our guild rules governs your behaviour within our guild">Guild Rules</a></li>
+					<li><a href="/legal/terms" title="These terms outline and govern your usage of our websites.">Terms of Service</a></li>
+					<li><a href="/legal/privacy" title="Our policies regarding data protection, privacy and our use of cookies.">Privacy &amp; Cookies</a></li>
+					<li><a href="/legal/battlenet" title="Our policies and source code relating to our usage of the battle.net API">Battle.net Usage Information</a></li>
+					<li><a href="http://www.benargo.co.uk/" target="_blank">Website by Ben Argo</a></li>
 				<p><span class="bold">Disclaimers:</span> World of Warcraft&trade;, Mists of Pandaria&trade; and Blizzard Entertainment&trade; are all trademarks or registered trademarks of Blizzard Entertainment SAS. internationally. All related materials, logos, and images are copyright &copy; Blizzard Entertainment SAS. Ashkandari is in no way associated with or endorsed by Blizzard Entertainment.</p>
 			</footer>
 			<div class="clear"></div>
