67583c06ccd84a8292acdcf81711091f234cf457
Ben Argo <ben@beanrgo.com>
2012-09-01 12:37:10 +0100

    Removing achievement points from alts as they are now account-wide

--- www/roster/character.php
+++ www/roster/character.php
@@ -143,7 +143,7 @@ if( $character = new character($character_id) ) {
 </table>
 	<?php if($character->isClaimed()) {
 		
-		?><h2>Alts</h2><?php
+		?><h2>Other Characters</h2><?php
 		
 		$alts = $character->getAlts();
 		
@@ -158,7 +158,6 @@ if( $character = new character($character_id) ) {
 				<img src="<?php echo $alt->getThumbnail(); ?>" alt="Character Thumbnail" class="float right" />
 				<p class="bold"><?php echo $alt->name; ?></p>
 				<p class="<?php echo $class->slug; ?>"><img src="<?php echo $alt->getRaceIcon(); ?>" alt="Race Icon" /> <img src="<?php echo $class->icon_url; ?>" alt="Class Icon" /> Level <?php echo $alt->level; ?> <?php echo $race->name; ?> <?php echo $class->name; ?></p>
-				<p>Achievement Points: <?php echo $alt->achievements; ?> <img src="/media/images/icons/achievements.gif" alt="Achievement points icon" class="noborder" /></p>
 			</a><?php
 			
 		}
